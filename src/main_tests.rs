2a60e008971c14591439042f31ae8b1648cbec55
John Lyon-Smith <jolyonsm@amazon.com>
2021-08-08 15:19:52 -0700

    Refactor tests

---
+++ src/main_tests.rs
@@ -0,0 +1,93 @@
+use tempfile::tempfile;
+
+mod tests {
+  use crate::*;
+
+  #[test]
+  fn test_read_eol_info_lf() {
+    let line_info = read_eol_info(&mut "\n".as_bytes()).unwrap();
+
+    assert_eq!(
+      line_info,
+      LineInfo {
+        cr: 0,
+        lf: 1,
+        crlf: 0,
+        num_lines: 2,
+        num_endings: 1
+      }
+    );
+  }
+
+  #[test]
+  fn test_read_eol_info_cr() {
+    let line_info = read_eol_info(&mut "\r".as_bytes()).unwrap();
+
+    assert_eq!(
+      line_info,
+      LineInfo {
+        cr: 1,
+        lf: 0,
+        crlf: 0,
+        num_lines: 2,
+        num_endings: 1
+      }
+    );
+  }
+
+  #[test]
+  fn test_read_eol_info_crlf() {
+    let line_info = read_eol_info(&mut "\r\n".as_bytes()).unwrap();
+
+    assert_eq!(
+      line_info,
+      LineInfo {
+        cr: 0,
+        lf: 0,
+        crlf: 1,
+        num_lines: 2,
+        num_endings: 1
+      }
+    );
+  }
+
+  #[test]
+  fn test_read_eol_info_mixed1() {
+    let line_info = read_eol_info(&mut "\n\r\n\r".as_bytes()).unwrap();
+
+    assert_eq!(
+      line_info,
+      LineInfo {
+        cr: 1,
+        lf: 1,
+        crlf: 1,
+        num_lines: 4,
+        num_endings: 3
+      }
+    );
+  }
+
+  #[test]
+  fn test_write_new_file() {
+    let mut input = "abc\n\r\r\n".as_bytes();
+    let mut output = Vec::new();
+    let num_lines = write_new_file(&mut input, &mut output, EndOfLine::CrLf).unwrap();
+
+    assert_eq!(num_lines, 4);
+    assert_eq!(String::from_utf8(output).unwrap(), "abc\r\n\r\n\r\n")
+  }
+
+  #[test]
+  #[should_panic]
+  fn test_write_new_file_bad_arg() {
+    let mut input = "".as_bytes();
+    let mut output = Vec::new();
+    write_new_file(&mut input, &mut output, EndOfLine::Auto).unwrap();
+  }
+
+  #[test]
+  fn test_run() {
+    let file = tempfile();
+
+    run(&mut input, &mut output, EndOfLine::Auto).unwrap();
+  }}
