82931be20ac883019d52a6806e9b8192b3c96cd5
John Lyon-Smith <john@lyon-smith.org>
2021-08-09 18:56:06 -0700

    Move ender into a workspace

--- get-coverage.sh
+++ get-coverage.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 export RUSTFLAGS="-Zinstrument-coverage"
-export LLVM_PROFILE_FILE="./scratch/$(whoami)-%p-%m.profraw"
+export LLVM_PROFILE_FILE="$(pwd)/scratch/$(whoami)-%p-%m.profraw"
 cargo test
 grcov . -s . --binary-path ./target/debug/ -t html --branch --ignore-not-existing -o ./target/debug/coverage/ --excl-start '^//\s*\{grcov-excl-start\}' --excl-stop '^//\s*\{grcov-excl-end\}'
