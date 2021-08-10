5aa6c6642fb05575b843f821bf29ddd521a7c47f
John Lyon-Smith <john@lyon-smith.org>
2021-08-09 18:31:50 -0700

    100% line coverage

--- src/main_tests.rs
+++ src/main_tests.rs
@@ -84,13 +84,13 @@ mod tests {
   }
 
   #[test]
-  fn test_run() {
+  fn test_run_auto() {
     let temp_dir = tempfile::tempdir().unwrap();
     let output_path = temp_dir.path().join("output_file.txt");
     let input_path = temp_dir.path().join("input_file.txt");
     let input_file = input_path.to_str().unwrap();
 
-    std::fs::write(input_file, "abc\n").unwrap();
+    std::fs::write(input_file, "abc\nxyz\r\n\r\n123\r\r\r").unwrap();
 
     run(
       input_file,
@@ -98,6 +98,51 @@ mod tests {
       Some(EndOfLine::Auto),
     )
     .unwrap();
+
+    temp_dir.close().unwrap();
+  }
+
+  #[test]
+  fn test_run_just_status() {
+    let temp_dir = tempfile::tempdir().unwrap();
+    let input_path = temp_dir.path().join("input_file.txt");
+    let input_file = input_path.to_str().unwrap();
+
+    std::fs::write(input_file, "abc\r\n").unwrap();
+
+    run(input_file, None, None).unwrap();
+
+    temp_dir.close().unwrap();
+  }
+
+  #[test]
+  fn test_run_crlf() {
+    let temp_dir = tempfile::tempdir().unwrap();
+    let output_path = temp_dir.path().join("output_file.txt");
+    let input_path = temp_dir.path().join("input_file.txt");
+    let input_file = input_path.to_str().unwrap();
+
+    std::fs::write(input_file, "abc\r\n").unwrap();
+
+    run(
+      input_file,
+      Some(output_path.to_str().unwrap()),
+      Some(EndOfLine::CrLf),
+    )
+    .unwrap();
+
+    temp_dir.close().unwrap();
+  }
+
+  #[test]
+  fn test_run_cr() {
+    let temp_dir = tempfile::tempdir().unwrap();
+    let input_path = temp_dir.path().join("input_file.txt");
+    let input_file = input_path.to_str().unwrap();
+
+    std::fs::write(input_file, "abc\r").unwrap();
+
+    run(input_file, None, Some(EndOfLine::CrLf)).unwrap();
 
     temp_dir.close().unwrap();
   }
